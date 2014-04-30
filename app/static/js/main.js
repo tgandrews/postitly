fec2bbd4a762d6c19c94c87ce6b0a7bb6b07f87a
Tom Andrews <git@tomandrews.co.uk>
2014-04-30 21:45:53 +0100

    Dragging and dropping is now working. Ugly workaround because of position absolute

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,1 +1,30 @@
+$('.draggable-source').draggable({ helper: 'clone'});
+$('.draggable-canvas').draggable();
 
+$('#canvas').droppable({
+  drop: function (event, ui) {
+    var $this = $(this);
+    var $item;
+    var position = { left: ui.position.left, top: ui.position.top };
+
+    if ($this.has(ui.draggable).length === 1) {
+      $item = $(ui.draggable);
+    }
+    else {
+      var $item = $(ui.draggable).clone();
+      $item.draggable();
+
+      $item.removeClass('draggable-source');
+      $item.addClass('draggable-canvas');
+      $item.css('position', 'absolute');
+      $this.append($item.get(0));
+
+      position.left = position.left - 200;
+    }
+
+    $item.css('left', position.left);
+    $item.css('top', position.top);
+
+    console.log(event, ui, $this);
+  }
+})
