cafc2fa6a6d26018417cbc731e41ab67760c72ca
Tom Andrews <git@tomandrews.co.uk>
2014-04-30 22:00:51 +0100

    Wrap in func to keep control of the local variables

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,30 +1,34 @@
-$('.draggable-source').draggable({ helper: 'clone'});
-$('.draggable-canvas').draggable();
+(function ($, window, undefined) {
+  var canvasDraggableOptions = { containment: 'parent' };
+  $('.draggable-source').draggable({ helper: 'clone'});
+  $('.draggable-canvas').draggable(canvasDraggableOptions);
 
-$('#canvas').droppable({
-  drop: function (event, ui) {
-    var $this = $(this);
-    var $item;
-    var position = { left: ui.position.left, top: ui.position.top };
+  $('#canvas').droppable({
+    drop: function (event, ui) {
+      var $this = $(this);
+      var $item;
+      var position = { left: ui.position.left, top: ui.position.top };
 
-    if ($this.has(ui.draggable).length === 1) {
-      $item = $(ui.draggable);
+      if ($this.has(ui.draggable).length === 1) {
+        $item = $(ui.draggable);
+      }
+      else {
+        var $item = $(ui.draggable).clone();
+        $item.draggable(canvasDraggableOptions);
+
+        $item.removeClass('draggable-source');
+        $item.addClass('draggable-canvas');
+        $item.css('position', 'absolute');
+        $item.find('p').attr('contenteditable', true);
+        $this.append($item.get(0));
+
+        position.left = position.left - 200;
+      }
+
+      $item.css('left', position.left);
+      $item.css('top', position.top);
+
+      console.log(event, ui, $this);
     }
-    else {
-      var $item = $(ui.draggable).clone();
-      $item.draggable();
-
-      $item.removeClass('draggable-source');
-      $item.addClass('draggable-canvas');
-      $item.css('position', 'absolute');
-      $this.append($item.get(0));
-
-      position.left = position.left - 200;
-    }
-
-    $item.css('left', position.left);
-    $item.css('top', position.top);
-
-    console.log(event, ui, $this);
-  }
-})
+  });
+})($, window)
