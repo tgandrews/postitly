6ebe65e225f85de2c2a37714b387859844541ccf
Tom Andrews <git@tomandrews.co.uk>
2014-04-30 23:30:46 +0100

    Reorganise JS for a little sanity

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,36 +1,3 @@
 (function ($, window, undefined) {
-  var canvasDraggableOptions = { containment: 'parent' };
-  $('.draggable-source').draggable({ helper: 'clone'});
-  $('.draggable-canvas').draggable(canvasDraggableOptions);
-
-  $('#canvas').droppable({
-    drop: function (event, ui) {
-      var $this = $(this);
-      var $item;
-      var position = { left: ui.position.left, top: ui.position.top };
-
-      if ($this.has(ui.draggable).length === 1) {
-        $item = $(ui.draggable);
-      }
-      else {
-        var $item = $(ui.draggable).clone();
-        $item.draggable(canvasDraggableOptions);
-
-        $item.removeClass('draggable-source');
-        $item.addClass('draggable-canvas');
-        $item.css('position', 'absolute');
-
-        $item.attr('contentEditable', 'true');
-
-        $this.append($item.get(0));
-
-        position.left = position.left - 200;
-      }
-
-      $item.css('left', position.left);
-      $item.css('top', position.top);
-
-      console.log(event, ui, $this);
-    }
-  });
+  window.postitly.draggable();
 })($, window)
