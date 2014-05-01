4be41e2fac9e52eca0821c9be6cf0f35e594dee9
Tom Andrews <git@tomandrews.co.uk>
2014-05-01 20:50:49 +0100

    Fix issue causing item to not be contained within canvas

--- app/static/js/modules/draggable.js
+++ app/static/js/modules/draggable.js
@@ -1,5 +1,5 @@
 (function ($, window, undefined) {
-  var canvasDraggableOptions = { continment: 'parent' };
+  var canvasDraggableOptions = { containment: 'parent' };
 
   var canvasDrop =  function (event, ui) {
     var $this = $(this);
