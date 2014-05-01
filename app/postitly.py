3cc3b3cc3cfe60e2bf7dd18d0651a6914a5e3cb6
Tom Andrews <git@tomandrews.co.uk>
2014-05-01 21:37:42 +0100

    Send 200 and log request

--- app/postitly.py
+++ app/postitly.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
   return render_template('home.html')
+
+@app.route('/notes', methods=['POST'])
+def create_note():
+  print request.form
+  return ''
 
 if __name__ == '__main__':
   app.run(debug=True)
