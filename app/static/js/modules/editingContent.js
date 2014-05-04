6676a53ae065d680f648f8d10886d022e05a927f
Tom Andrews <git@tomandrews.co.uk>
2014-05-04 18:29:45 +0100

    Fix issues around adding images and being able delete the HTML of the post it meaning it lost the corner and span to contain the contents

--- app/static/js/modules/editingContent.js
+++ app/static/js/modules/editingContent.js
@@ -8,21 +8,32 @@
       }
     },
     makeEditable: function (item) {
-      item.setAttribute('contenteditable', 'true');
-      item.addEventListener('blur', editingContent.onBlur);
-      item.addEventListener('click', editingContent.onClick);
+      // Cannot make editable something if it is display none
+      if (!item.classList.contains('img-added')) {
+        var span = item.querySelector('span');
+        span.setAttribute('contenteditable', 'true');
+        span.addEventListener('blur', editingContent.onBlur);
+        item.addEventListener('click', editingContent.onClick);
+      }
     },
     onBlur: function () {
-      var item = window.postitly.itemStore.createItemFromElement(this);
+      var itemElement = this.parentElement.parentElement;
+      itemElement.classList.remove('focus');
+
+      var item = window.postitly.itemStore.createItemFromElement(itemElement);
       var text = item.text;
       if (item.type === 'img' && text.indexOf('http') === 0) {
-        this.style.backgroundImage = "url('" + text + "')";
-        this.classList.add('img-added');
+        itemElement.style.backgroundImage = "url('" + text + "')";
+        itemElement.classList.add('img-added');
+        this.removeAttribute('contenteditable')
       }
-      window.postitly.itemStore.update(this);
+      window.postitly.itemStore.update(itemElement);
     },
     onClick: function () {
-      this.focus();
+      if (!this.classList.contains('img-added')) {
+        this.querySelector('span').focus();
+        this.classList.add('focus');
+      }
     }
   }
 
